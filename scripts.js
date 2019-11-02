1834c851fa0da567b3f6ddb93e6fc7a8810d3a92
Caleb <whitlowc@otc.edu>
2019-11-01 23:10:45 -0500

    Api Call

--- scripts.js
+++ scripts.js
@@ -1,16 +1,16 @@
 
-Vue.component('landing',{
+Vue.component('landing', {
     template:
-    `
+        ``
     
     `
     
 
 })
 
+app.getJobs();
 
-
-var app = new VTTCue({
+var app = new Vue({
     el: "#app",
     data:{
         location: "",
@@ -19,6 +19,22 @@ var app = new VTTCue({
 
     },
     methods:{
+        //get JOBS
+        getJobs: function() {
+            var httpRequest = new XMLHttpRequest();
+            var token = "iyOSd0gsuR9TZIqWe9wAWuRbLai0HYCmLG3OrUFfFct1ePozfiCoZlOVKVfqfTMGung2IxC9LY2WGZUf"
+            httpRequest.open("get", `https://jobs.api.sgf.dev/api/job?api_token='${token}`);
+            httpRequest.send(null);
+            httpRequest.onreadystatechange = loadJobs;
 
+            function loadJobs() {
+                if (httpRequest.readyState == 4 && httpRequest.status == 200) {
+                    var jobs = httpRequest.responseText;
+                    console.log(jobs);
+                    //var jsObject = JSON.parse(launches);
+                }
+            }
+        }
     }
 })
+
