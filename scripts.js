3359325a74160582f106b4a34c132264c0276b35
Caleb <whitlowc@otc.edu>
2019-11-01 23:12:48 -0500

    getJobs
    Moved getJobs outside of Vue

--- scripts.js
+++ scripts.js
@@ -3,12 +3,25 @@ Vue.component('landing', {
     template:
         ``
     
-    `
-    
 
 })
 
-app.getJobs();
+ //get JOBS
+ getJobs: function() {
+    var httpRequest = new XMLHttpRequest();
+    var token = "iyOSd0gsuR9TZIqWe9wAWuRbLai0HYCmLG3OrUFfFct1ePozfiCoZlOVKVfqfTMGung2IxC9LY2WGZUf"
+    httpRequest.open("get", `https://jobs.api.sgf.dev/api/job?api_token='${token}`);
+    httpRequest.onreadystatechange = loadJobs;
+    
+    function loadJobs() {
+        
+        if (httpRequest.readyState == 4 && httpRequest.status == 200) {
+            var jobs = httpRequest.responseText;
+            console.log(jobs);
+            //var jsObject = JSON.parse(launches);
+        }
+    }
+}
 
 var app = new Vue({
     el: "#app",
@@ -19,22 +32,9 @@ var app = new Vue({
 
     },
     methods:{
-        //get JOBS
-        getJobs: function() {
-            var httpRequest = new XMLHttpRequest();
-            var token = "iyOSd0gsuR9TZIqWe9wAWuRbLai0HYCmLG3OrUFfFct1ePozfiCoZlOVKVfqfTMGung2IxC9LY2WGZUf"
-            httpRequest.open("get", `https://jobs.api.sgf.dev/api/job?api_token='${token}`);
-            httpRequest.send(null);
-            httpRequest.onreadystatechange = loadJobs;
-
-            function loadJobs() {
-                if (httpRequest.readyState == 4 && httpRequest.status == 200) {
-                    var jobs = httpRequest.responseText;
-                    console.log(jobs);
-                    //var jsObject = JSON.parse(launches);
-                }
-            }
-        }
+       
     }
 })
+
+
 
