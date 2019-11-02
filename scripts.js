a61014ee030c3ad557e8bc5f401bc540cb8740ec
Joey Brosam <joebro2001@gmail.com>
2019-11-02 16:45:39 -0700

    added return to location

--- scripts.js
+++ scripts.js
@@ -138,6 +138,7 @@ var settings = {
                     <input type="checkbox" class="custom-control-input" id="customSwitch4">                    
                 </div>
             </div>
+            <input @click="component('locationPage')" class="btn" type="button" value="location">
         </section>
     `,
     props:['component']
